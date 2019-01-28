a399c08d8e3c725285dd040843285bc9d9043c12
yenertuz <yenertuz@hotmail.com>
2019-01-28 14:48:58 -0800

    update calculator.jsx

---
+++ calculator.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Calculator extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+				<h1 style={{backgroundColor: "red", color: "yellow"}}>Hello there!</h1>
+			</div>
+		);
+	}
+}
+
+export default Calculator;
